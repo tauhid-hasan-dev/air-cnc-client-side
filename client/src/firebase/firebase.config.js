f52bc8cfc1bdf2e00e2ce2d1c4f3834c89a0d18f
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-21 13:34:06 +0300

    first commit

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -2,6 +2,12 @@ import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
   // COnfig code goes here
+  apiKey: "AIzaSyAGpHj6MsP-m9HHiFWEqyffxnYDyz0T0-Q",
+  authDomain: "air-cnc-2ee51.firebaseapp.com",
+  projectId: "air-cnc-2ee51",
+  storageBucket: "air-cnc-2ee51.appspot.com",
+  messagingSenderId: "206904374389",
+  appId: "1:206904374389:web:d1ca0d60b2419c405f343c"
 }
 
 const app = initializeApp(firebaseConfig)
